e32504505aa4dfa0f5c540fbe1317411653021dd
cosmic-boy0111 <87473128+cosmic-boy0111@users.noreply.github.com>
2022-12-26 14:28:59 +0530

    update

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(myParser.urlencoded({limit: '200mb', extended: true}));
 app.use(myParser.text({ limit: '200mb' }));
 
 app.use(require('./router/auth'));
-// app.use(require('./router/Problems/ProblemsRoutes'));
-// app.use(require('./router/Problems/ProblemSolutionRoutes'))
+app.use(require('./router/Problems/ProblemsRoutes'));
+app.use(require('./router/Problems/ProblemSolutionRoutes'))
 app.use(require('./router/UserAction/Followers'))
 app.use(require('./router/Blog/Blog.js'))
 app.use(require('./router/Users/userList'))
